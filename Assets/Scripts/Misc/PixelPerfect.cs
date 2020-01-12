db1df7e36230e2322a4ff672b68d134e42f7c5a8
BuildTools <unconfigured@null.spigotmc.org>
2020-01-12 13:28:03 -0800

    First commit - tilemap/terrain

---
+++ Assets/Scripts/Misc/PixelPerfect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfect : MonoBehaviour
+{
+    public int PPU = 16;
+    public float PPUScale = 1;
+
+    void Update()
+    {
+        gameObject.GetComponent<Camera>().orthographicSize = (Screen.height / (PPUScale * PPU)) * 0.5f;
+    }
+}
---
+++ Assets/Scripts/Tilemap/GenerateTilemap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GenerateTilemap : MonoBehaviour
+{
+    private Tilemap map;
+
+    public int worldWidth = 10;
+    public int worldHeight = 10;
+
+    public Tile dirt;
+    
+    void Start()
+    {
+        map = gameObject.GetComponent<Tilemap>();
+
+        generateTopLevelTerrain(1);
+    }
+
+    void generateTopLevelTerrain(int seed)
+    {
+        for (int i = -(worldWidth / 2); i < worldWidth / 2; i++)
+        {
+            for (int j = -worldHeight; j < 0; j++)
+            {
+                map.SetTile(new Vector3Int(i, j, 0), dirt);
+            }
+        }
+    }
+}
