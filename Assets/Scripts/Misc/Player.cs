7c942cf50efeffa5483c84769e3d3b3bf7f62f3e
theketchupdude <tom@ascholer.org>
2020-01-12 14:13:16 -0800

    Terrain Generation work

---
+++ Assets/Scripts/Misc/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController character;
+
+    public int Speed = 1;
+
+    void Start()
+    {
+        character = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        character.Move(move * Time.deltaTime * Speed);
+        if (move != Vector3.zero)
+            transform.forward = move;
+    }
+}
--- Assets/Scripts/Tilemap/GenerateTilemap.cs
+++ Assets/Scripts/Tilemap/GenerateTilemap.cs
@@ -11,16 +11,28 @@ public class GenerateTilemap : MonoBehaviour
     public int worldHeight = 10;
 
     public Tile dirt;
-    
+
+    public float noiseScale = 1;
+    public int seed = 1;
+    public Vector2 offset = new Vector2(0, 0);
+
+    public float range = 1;
+
     void Start()
     {
         map = gameObject.GetComponent<Tilemap>();
 
-        generateTopLevelTerrain(1);
+        generateTopLevelTerrain();
     }
 
-    void generateTopLevelTerrain(int seed)
+    void generateTopLevelTerrain()
     {
+        float[] noiseMap = Noise.GenerateNoiseMap(worldWidth, seed, noiseScale + 0.0001f, offset);
+        for (int i = 0; i < worldWidth; i++)
+        {
+            noiseMap[i] = noiseMap[i] * (Mathf.Abs(range) * 2) - (Mathf.Abs(range));
+        }
+
         for (int i = -(worldWidth / 2); i < worldWidth / 2; i++)
         {
             for (int j = -worldHeight; j < 0; j++)
